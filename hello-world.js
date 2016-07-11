a68d92c0939cf671f78ec1b6e8febae338d98ede
pauline <pauline_rousseau@hotmail.co.uk>
2016-07-11 18:17:42 +0000

    hello world

--- hello-world.js
+++ hello-world.js
@@ -1,5 +1,20 @@
+// first programm
 
 console.log ("Hello World!");
 
 setTimeout(function(){console.log("Hello World Again!")},10000);
+
+//Write a piece of code using setTimeout that 
+//prints “Hello World!” every 10 seconds forever.
+
+function forever (){console.log ("Hello World!")
+    setTimeout(forever, 10000);
+};
+
+forever();
+
+
+
+
+
 
